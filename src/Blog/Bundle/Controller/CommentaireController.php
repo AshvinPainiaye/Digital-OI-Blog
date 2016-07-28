3c50c18cb8ee47fd2ede8eb91fcb63d709aa395f
AshFromReunion <ash-concept@hotmail.fr>
2016-07-28 16:34:27 +0400

    maj like

--- src/Blog/Bundle/Controller/CommentaireController.php
+++ src/Blog/Bundle/Controller/CommentaireController.php
@@ -10,6 +10,9 @@ use Blog\Bundle\Entity\Commentaire;
 use Blog\Bundle\Form\CommentaireType;
 
 
+use Blog\Bundle\Entity\Article;
+
+
 /**
 * Commentaire controller.
 *
@@ -185,65 +188,13 @@ class CommentaireController extends Controller
   */
   public function jaimePostesAction()
   {
-    $em = $this->getDoctrine()->getManager();
-
     $user = $this->getUser();
-    $like = $user->getCommentairelike();
-
-    $commentaires = $em->getRepository('BlogBundle:Commentaire')->findBy(['user'=>$user]);
-
-
-    return $this->render('commentaire/index.html.twig', array(
-      'commentaires' => $commentaires,
-      'user' => $user,
+    $userId = $user->getId();
+    $comments = $user->getCommentairelike();
+    return $this->render('commentaire/jaimePostes.html.twig', array(
+      'commentaires' => $comments,
     ));
-
-
-
-
-  // /**
-  // * Lists all Commentaire entities.
-  // *
-  // * @Route("/jaime/postes/{id}", name="jaime_postes")
-  // * @Method("GET")
-  // */
-  // public function jaimePostesAction()
-  // {
-  //   // $userId = $this->getUser();
-  //   // $userId->getId();
-  //      $user = $this->getUser();
-  //
-  //      $like = $user->getCommentairelike();
-  //
-  //      $em = $this->getDoctrine()->getManager();
-  //
-  //      $usersRepository = $this->getDoctrine()
-  //               ->getRepository('BlogBundle:User');
-  //       $listeUsers = $usersRepository->findAll();
-  //
-  //    $utilisateur = $em->getRepository('BlogBundle:User')->findBy(['id'=>$user]);
-  //   $commentaires = $em->getRepository('BlogBundle:Commentaire')->findBy(['user'=>$user]);
-  //
-  //
-  //   return $this->render('commentaire/jaimePostes.html.twig', array(
-  //     'commentaires' => $commentaires,
-  //     'user' => $user,
-  //     'id' => $user,
-  //     'listeUsers' => $listeUsers
-  //   ));
-
-
-
-
-
   }
-
-
-
-
-
-
-
 
 
 
