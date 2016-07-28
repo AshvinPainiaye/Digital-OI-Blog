82240ca58181aa7fd95e904730c4aa838f10abfe
AshFromReunion <ash-concept@hotmail.fr>
2016-07-28 23:33:31 +0400

    ajout fleche pour remonter la page, balise pour la referencement, supprimer article depuis mes articles, fix bug affichage changepassword

--- web/js/main.js
+++ web/js/main.js
@@ -1,21 +1,36 @@
 
 $(document).ready(function () {
 
-  // $(window).scroll(
-  //   function () {
-  //     if ($(window).scrollTop() > $("header").offset().top) {
-  //       $('#fix').addClass("navbar-fixed");
-  //     } else {
-  //       $('#fix').removeClass("navbar-fixed");
-  //     }
-  //   }
-  // );
-
-
   //Preloader
   $(window).load(function () {
     $('#page-loader').fadeOut(500);
   });
   //fin
+
+
+  //affiche icone remonter
+  $(window).scroll(
+    function () {
+      if ($(window).scrollTop() > 1) {
+        // fixed
+        $(".icon-return-top").css('visibility', 'visible');
+      } else {
+        // unfixed
+        $(".icon-return-top").css('visibility', 'hidden');
+      }
+    }
+  );
+//fin
+
+
+  $('a[href^="#returnTop"]').click(function () {
+    var the_id = $(this).attr("href");
+
+    $('html, body').animate({
+      scrollTop: $(the_id).offset().top
+    }, 'slow');
+    return false;
+  });
+
 
 });
