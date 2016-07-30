6e582c147b9262b3eecb6f0e587102387f066156
davidatc <atchiaman.david@hotmail.fr>
2016-07-30 14:41:41 +0400

    message

--- src/Blog/Bundle/Entity/User.php
+++ src/Blog/Bundle/Entity/User.php
@@ -4,7 +4,6 @@ namespace Blog\Bundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
@@ -38,6 +37,13 @@ class User extends BaseUser
     * @var string
     */
     private $imageName;
+
+    /**
+        * @ORM\Column(type="datetime", nullable=true)
+        *
+        * @var \DateTime
+        */
+       private $updatedAt;
 
     /**
     * @ORM\Column(type="string", length=255)
@@ -112,26 +118,28 @@ class User extends BaseUser
     }
 
     /**
-    * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
-    * of 'UploadedFile' is injected into this setter to trigger the  update. If this
-    * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
-    * must be able to accept an instance of 'File' as the bundle will inject one here
-    * during Doctrine hydration.
-    *
-    * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
-    *
-    * @return Product
-    */
-    public function setImageFile(File $image = null)
-    {
-      $this->imageFile = $image;
-      if ($image) {
-        // It is required that at least one field changes if you are using doctrine
-        // otherwise the event listeners won't be called and the file is lost
-        $this->date = new \DateTime('now');
-      }
-      return $this;
+ * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+ * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+ * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+ * must be able to accept an instance of 'File' as the bundle will inject one here
+ * during Doctrine hydration.
+ *
+ * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+ *
+ * @return Product
+ */
+public function setImageFile(File $image = null)
+{
+    $this->imageFile = $image;
+
+    if ($image) {
+         // It is required that at least one field changes if you are using doctrine
+         // otherwise the event listeners won't be called and the file is lost
+         $this->updatedAt = new \DateTime('now');
     }
+
+    return $this;
+}
     /**
     * @return File
     */
@@ -326,5 +334,29 @@ class User extends BaseUser
     public function getArticlelike()
     {
         return $this->articlelike;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return User
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
     }
 }
